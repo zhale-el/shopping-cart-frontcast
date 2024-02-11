4745dcf3febdc48dd24455cb2545c14f00e3c654
zhaleh <zhalehelmi@gmail.com>
2024-02-11 15:41:20 +0330

    add function renderProducts

--- app.js
+++ app.js
@@ -12,3 +12,24 @@ const products = [
     image: "./images/airpods.png",
   },
 ];
+const renderProducts = () => {
+  const productDiv = document.querySelector(".products");
+  productDiv.innerHTML = "";
+
+  products.forEach((item, index) => {
+    productDiv.innerHTML += `
+
+   <div class="product">
+    <div class="product__image">
+      <img src= ${item.image}/>
+    </div>
+    <h2 class="product__title">${item.name}</h2>
+    <h3 class="product__price">${item.price} تومان</h3>
+    <button class="add-to-cart">افزودن به سبد خرید</button>
+  </div>
+
+    `;
+  });
+};
+
+renderProducts();
