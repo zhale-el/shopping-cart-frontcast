80b0c49ed48ae112ab3cf3ec32160e418cc55846
zhaleh <zhalehelmi@gmail.com>
2024-02-11 15:39:26 +0330

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12",
+    price: 99,
+    image: "./images/iphone-12.png",
+  },
+  {
+    id: 2,
+    name: "AirPods",
+    price: 89,
+    image: "./images/airpods.png",
+  },
+];
